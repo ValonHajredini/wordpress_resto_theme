79b8753f53cce14b20c403dcd2a91e2c11b30b1e
Valon Hajredini <hajredini.valon@gmail.com>
2016-10-21 00:20:36 +0200

    Design the 404.php and search.php

--- 404.php
+++ 404.php
@@ -11,40 +11,39 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<div class="wrapper">
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'resto-theme' ); ?></h1>
 				</header><!-- .page-header -->
-
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'resto-theme' ); ?></p>
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'resto-theme' ); ?></p>
 
-					<?php
+						<?php
 						get_search_form();
 
 						the_widget( 'WP_Widget_Recent_Posts' );
 
 						// Only show the widget if site has multiple categories.
 						if ( resto_theme_categorized_blog() ) :
-					?>
+							?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'resto-theme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'resto-theme' ); ?></h2>
+								<ul>
+									<?php
+									wp_list_categories( array(
+										'orderby'    => 'count',
+										'order'      => 'DESC',
+										'show_count' => 1,
+										'title_li'   => '',
+										'number'     => 10,
+									) );
+									?>
+								</ul>
+							</div><!-- .widget -->
 
-					<?php
+							<?php
 						endif;
 
 						/* translators: %1$s: smiley */
@@ -52,11 +51,11 @@ get_header(); ?>
 						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
 						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+						?>
 
-				</div><!-- .page-content -->
+					</div><!-- .page-content -->
 			</section><!-- .error-404 -->
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
